7a44a7cb71a2310ad001b23134856c7a59982038
Gamaliel Quiroz <83394687+ghami20@users.noreply.github.com>
2023-05-09 10:40:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+window.addEventListener('load', function() {
+
+
+
+
+
+
+  let lista =  JSON.parse(localStorage.getItem("lista"));
+  console.log(lista);
+
+   lista.forEach(element => {
+      let id= lista.indexOf(element);
+      console.log(id);
+      let elementos = document.getElementById("Elementos");
+      let div = document.createElement("Button");
+      div.innerHTML = element.nombre +" " + element.apellido;
+      div.className += "Midiv";
+      div.addEventListener("click", cambiarcolor, false);
+      elementos.appendChild(div);
+
+      
+   });
+});
+
+
+const cambiarcolor=(e)=>{
+    console.log(e.target.className);
+    let elementos = document.getElementById("Elementos");
+    
+    elementos.removeChild(e.target);
+}
+   
+   
+const agregarElemento = () => {
+
+   var name = document.getElementById("nombre").value;
+   var lastname = document.getElementById("apellido").value; 
+   console.log(name);
+if(name.length>0 ){
+
+   var ingreso = {nombre: name , apellido: lastname};
+   
+   
+   console.log(ingreso);
+   let lista =  JSON.parse(localStorage.getItem("lista"));
+   lista.push(ingreso);
+   console.log(lista);
+   localStorage.setItem("lista",JSON.stringify(lista));
+   
+   window.location. reload();
+}else{
+
+   alert("El nombre no contiene nada");
+}
+
+}
+const eliminarElemento = () => {
+
+   var name = document.getElementById("nombre").value;
+if(name.length>0 ){
+
+   let lista =  JSON.parse(localStorage.getItem("lista"));
+   lista.forEach(element => {
+      if(element.name==name){
+         delete(lista[lista.indexOf(element)])
+      }
+      alert("Elemento eliminado");
+      window.location. reload();
+      return;
+   });
+  
+
+
+}else{
+
+   alert("no se encontró");
+}
+
+}
